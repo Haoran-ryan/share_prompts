013fb9bd8c565d9cb83b52fb4284167a36695e93
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-10-11 08:53:14 +1100

    promptcardlist created

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+'use client';
+import { useState } from "react";
+import PromptCard from "./PromptCard";
+// single-use component
+const PromptCardList = ({data, handleTagClick}) =>{
+  <div className="mt-16 prompt_layout">
+    {data.map((post)=>(
+        <PromptCard 
+          key={post.id}
+          id={post.id}
+          handleTagClick={handleTagClick}
+          />
+      )
+    )}
+  </div>
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+
+  };
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text" 
+        className="search_input peer" 
+        placeholder="Search for a tag or a username"
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        />
+      </form>
+    </section>
   )
 }
 
